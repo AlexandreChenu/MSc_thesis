35443fb4b004792e77d44b65ca74b7f8e29eda4f
Alexandre Chenu <mobby@dyn3190-213.wlan.ic.ac.uk>
2019-05-24 17:05:24 +0100

    changed bestfitnn for bestfit

--- examples2/simple_ebn.cpp
+++ examples2/simple_ebn.cpp
@@ -134,7 +134,8 @@ int main(int argc, char **argv)
     typedef eval::Parallel<Params> eval_t; //parallel eval (faster)
  
     typedef boost::fusion::vector< 
-        stat::BestFitNN<phen_t, Params>, 
+        //stat::BestFitNN<phen_t, Params>, 
+        stat::BestFit<phen_t, Params>,
         stat::QdContainer<phen_t, Params>, 
         stat::QdProgress<phen_t, Params> 
         >
