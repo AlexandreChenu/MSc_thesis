ca69b52ec22af37fa23526afff7beafc37738f23
Alexandre Chenu <mobby@dyn3180-39.wlan.ic.ac.uk>
2019-07-23 14:23:17 +0100

    exp_sampling_stoch

--- exp_sampling/best_fit_it.hpp
+++ exp_sampling/best_fit_it.hpp
@@ -11,7 +11,7 @@
 #include <boost/archive/binary_oarchive.hpp>
 #include <boost/archive/binary_iarchive.hpp>
 
-#include "/git/sferes2/exp/exp_sampling/fit_behav_new.hpp"
+#include "/git/sferes2/exp/exp_sampling/fit_behav_stoch.hpp"
 
 namespace sferes {
   namespace stat {
